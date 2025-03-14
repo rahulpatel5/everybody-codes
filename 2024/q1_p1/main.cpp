6b67712d660ac015814646c8524673261f9cbeae
rahulpatel5 <96894089+rahulpatel5@users.noreply.github.com>
2025-03-14 19:47:12 +0000

    2024 q1p1 | add solution

---
+++ 2024/q1_p1/main.cpp
@@ -0,0 +1,21 @@
+#include "q1p1.h"
+#include "input.h"
+#include "../../shared/StringUtils.h"
+#include "../../shared/Timer.h"
+#include <iostream>
+
+int main()
+{
+    Timer timer {};
+    
+    constexpr std::string_view input {input::input};
+    
+    constexpr std::string_view testInput {R"(ABBAC)"};
+    
+    int potions {q1p1::getPotionsNeeded(input)};
+    std::cout << "Number of potions needed: " << potions << '\n';
+    
+    std::cout << "Time taken: " << timer.getDuration() << " s\n";
+    
+    return 0;
+}
---
+++ 2024/q1_p1/q1p1.h
@@ -0,0 +1,25 @@
+#ifndef Q1P1_H
+#define Q1P1_H
+
+#include <string_view>
+#include <array>
+#include <string>
+#include <tuple>
+
+namespace q1p1
+{
+    constexpr int getPotionsNeeded(const std::string_view& area)
+    {
+        int potions {0};
+        for (char p : area)
+        {
+            if (p == 'B')
+                potions += 1;
+            else if (p == 'C')
+                potions += 3;
+        }
+        return potions;
+    }
+}
+
+#endif // Q1P1_H
